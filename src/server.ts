3adeae53a2f81c1e7d78631e0d44b3128c1fa7a6
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-12-22 20:54:24 -0300

    feat: add server initial structure

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  return res.json({
+    message: 'Hello world',
+  })
+})
+
+app.listen(3000, () => console.log('Server is running'))
