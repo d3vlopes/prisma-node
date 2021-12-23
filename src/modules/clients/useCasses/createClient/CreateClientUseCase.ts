eb456f09f6b02b9ca6f7fa90f22e18a566cbe82d
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-12-22 21:14:48 -0300

    feat: add CreateClientUseCase

---
+++ src/modules/clients/useCasses/createClient/CreateClientUseCase.ts
@@ -0,0 +1,39 @@
+import { hash } from 'bcrypt'
+
+import { prisma } from '../../../../database/prismaClient'
+
+interface ICreateClient {
+  username: string
+  password: string
+}
+
+export class CreateClientUseCase {
+  async execute({ username, password }: ICreateClient) {
+    // verifica se o usuário existe
+    const clientExist = await prisma.clients.findFirst({
+      where: {
+        username: {
+          // ignora se tem tem maiúsculo
+          mode: 'insensitive',
+        },
+      },
+    })
+
+    if (clientExist) {
+      throw new Error('Client already exists')
+    }
+
+    // criptografar a senha
+    const hashPassword = await hash(password, 10)
+
+    //salvar o cliente
+    const client = await prisma.clients.create({
+      data: {
+        username,
+        password: hashPassword,
+      },
+    })
+
+    return client
+  }
+}
