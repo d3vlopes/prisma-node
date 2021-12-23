4cd414264a3d0614e7d3e67e43e3753766a5ccf6
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-12-22 21:15:29 -0300

    feat: add routes on server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import express from 'express'
 
+import { routes } from './routes'
+
 const app = express()
+
+app.use(express.json())
+
+app.use(routes)
 
 app.get('/', (req, res) => {
   return res.json({
